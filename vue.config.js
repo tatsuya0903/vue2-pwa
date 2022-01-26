ed644cdd91632ffdca140d8d4201e1c4beb71f7b
tatsuya0903 <tama.and.pochi.0903@gmail.com>
2022-01-26 22:43:31 +0900

    ページタイトルを設定

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  pages: {
+    index: {
+      entry: 'src/main.ts', // 必須パラメータ
+      title: process.env.VUE_APP_TITLE,
+    },
+  },
   publicPath: process.env.VUE_APP_PUBLIC_PATH,
   productionSourceMap: process.env.NODE_ENV !== 'production',
 }
